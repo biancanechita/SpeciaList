3fb0e7d3a62d906ec140f2cd6c2c063c84d7621b
Bianca Nechita <bianca_nechita@beenear.com>
2023-05-26 21:05:22 +0300

    Registering Routes - closes #2

--- specialist-ui/src/router/index.js
+++ specialist-ui/src/router/index.js
@@ -1,26 +1,52 @@
 // Composables
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from "vue-router";
 
 const routes = [
   {
-    path: '/',
-    component: () => import('@/layouts/default/Default.vue'),
+    path: "/",
+    component: () => import("@/layouts/default/Default.vue"),
     children: [
       {
-        path: '',
-        name: 'Home',
+        path: "",
+        name: "Home",
         // route level code-splitting
         // this generates a separate chunk (about.[hash].js) for this route
         // which is lazy-loaded when the route is visited.
-        component: () => import(/* webpackChunkName: "home" */ '@/views/Home.vue'),
+        component: () =>
+          import(/* webpackChunkName: "home" */ "@/views/Home.vue"),
+      },
+      {
+        path: "/clinics",
+        component: null,
+        children: [
+          { path: "", name: "Clinics", component: null },
+          { path: "/:id", name: "ClinicDetailsPage", component: null },
+        ],
+      },
+      {
+        path: "/doctors",
+        component: null,
+        children: [
+          { path: "", name: "Doctors", component: null },
+          { path: "/:id", name: "DoctorProfilePage", component: null },
+        ],
+      },
+      {
+        path: "/specializations",
+        component: null,
+        children: [
+          { path: "", name: "Specializations", component: null },
+          { path: "/:id", name: "SpecializationDetailsPage", component: null },
+        ],
       },
     ],
   },
-]
+  { path: "/:404(.*)", component: null },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
-})
+});
 
-export default router
+export default router;
