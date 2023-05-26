903023f624901910913d3b850b86494a55b85836
Bianca Nechita <bianca_nechita@beenear.com>
2023-05-26 23:28:37 +0300

    Create Doctor availability management microservice - closes #7

---
+++ doctor-availability-management-microservice/src/test/java/ro/uaic/info/doctoravailabilitymanagementmicroservice/DoctorAvailabilityManagementMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.uaic.info.doctoravailabilitymanagementmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoctorAvailabilityManagementMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
