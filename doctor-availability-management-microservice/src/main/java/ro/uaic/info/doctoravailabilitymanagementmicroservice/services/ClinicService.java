6e0f93347b6fe4745dfca3b4f1c2e2f4448a9bdc
Bianca Nechita <bianca_nechita@beenear.com>
2023-05-27 13:29:59 +0300

    The Service Layer - closes #15

---
+++ doctor-availability-management-microservice/src/main/java/ro/uaic/info/doctoravailabilitymanagementmicroservice/services/ClinicService.java
@@ -0,0 +1,48 @@
+package ro.uaic.info.doctoravailabilitymanagementmicroservice.services;
+
+import lombok.RequiredArgsConstructor;
+import org.mapstruct.factory.Mappers;
+import org.springframework.stereotype.Service;
+import ro.uaic.info.doctoravailabilitymanagementmicroservice.beans.ClinicDTO;
+import ro.uaic.info.doctoravailabilitymanagementmicroservice.entities.Clinic;
+import ro.uaic.info.doctoravailabilitymanagementmicroservice.mappers.ClinicMapper;
+import ro.uaic.info.doctoravailabilitymanagementmicroservice.repositories.ClinicRepository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
+@Service
+public class ClinicService {
+
+    private final ClinicRepository clinicRepository;
+    private final ClinicMapper clinicMapper = Mappers.getMapper(ClinicMapper.class);
+
+    public List<ClinicDTO> findAll() {
+        return clinicRepository
+                .findAll()
+                .stream()
+                .map(clinicMapper::clinicToClinicDTO)
+                .collect(Collectors.toList());
+    }
+
+    public ClinicDTO findById(Long id) {
+        return clinicRepository
+                .findById(id)
+                .map(clinicMapper::clinicToClinicDTO)
+                .orElse(null);
+    }
+
+    public ClinicDTO save(ClinicDTO clinicDTO) {
+        Clinic clinic = clinicMapper.clinicDTOToClinic(clinicDTO);
+        return Optional.of(clinic)
+                .map(clinicRepository::save)
+                .map(clinicMapper::clinicToClinicDTO)
+                .orElse(null);
+    }
+
+    public void deleteById(Long id) {
+        clinicRepository.deleteById(id);
+    }
+}
